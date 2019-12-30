dc373deb08ffe246ab4a0e68c51705d02623b250
djarora19 <divyangkumar.arora@gmail.com>
2019-12-30 21:51:30 +0530

    Initial project setup

---
+++ src/main/java/adj/learn/springboot/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package adj.learn.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
